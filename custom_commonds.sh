28adfaa85ba4bb7ca2e17f911c659e43fd7455cf
ymm <ymm@ymmdeMac-mini.local>
2023-05-30 14:13:17 +0800

    cm:unzip增加-o参数,覆盖原有文件

--- custom_commonds.sh
+++ custom_commonds.sh
@@ -6,17 +6,23 @@ tar -xvpzf /cdrom/extra-data/backup.tar.gz -C /target/ --numeric-owner
 
 # 升级软件包
 soft_package=$(ls *md5*.zip)
+target_path=""
 if [ -f "$soft_package" ]; then
     echo "$soft_package"
 
     # 还原覆盖软件
     firewall_path="/target/opt/firewall"
     if [ -d "$firewall_path" ]; then
-        unzip $soft_package -d $firewall_path
+        target_path=$firewall_path
     fi
 
     audit_path="/target/opt/audit"
     if [ -d "$audit_path" ]; then
-        unzip $soft_package -d $audit_path
+        target_path=$audit_path
     fi
+
+    if [ -f "$target_path" ]; then
+        unzip -o $soft_package -d $target_path
+    fi
+
 fi
